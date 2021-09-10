f9a5228114c922d8111b879b1e475cde286e6765
MertcanYildirim078 <99068314@mydavinci.nl>
2021-09-10 12:57:16 +0200

    person.py

---
+++ person.py
@@ -0,0 +1,18 @@
+Naam = input('Voer hier uw naam in')
+Adres = input('Voer hier uw Adres in')
+Postcode = input('Voer hier uw Postcode in')
+Woonplaats = input('Voer hier uw Woonplaats')
+  
+
+
+print("----------------------------------------------------" + "\n"
+"Naam :" + Naam + "\n"                       
+"Adres :" + Adres + "\n"          
+"Postcode" + Postcode + "\n"                            
+"Woonplaats:" + Woonplaats + "\n"                         
+"----------------------------------------------------"
+) 
+
+
+
+
