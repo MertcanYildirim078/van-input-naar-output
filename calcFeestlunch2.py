f6dbb229d6d5cb3ec5201583d92fb30a43b31bfa
MertcanYildirim078 <99068314@mydavinci.nl>
2021-09-13 09:31:56 +0200

    croissant

---
+++ calcFeestlunch2.py
@@ -0,0 +1,7 @@
+croissant = input('Voer de waarde in van de croissant')
+stokbrood = input('Voer de waarde in van de stokbrood')
+kortingsbon = input('Voer de waarde in van de kortingsbon')
+
+print (croissant + " cent ter waarde van de croissant")  
+print(stokbrood + " cent ter waarde van de stokbrood")
+print(kortingsbon + " cent ter waarde van de kortingsbon")
